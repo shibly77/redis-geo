9655fb18b79784f220965e1b46fb7546e5d7e719
Nayem Ahmed Shibly <siblee77@outlook.com>
2018-09-11 16:39:09 +0600

    Region changed

--- deploy-envs.sh
+++ deploy-envs.sh
@@ -4,7 +4,7 @@
 export IMAGE_NAME=netcoreapps-redisgeo
 export IMAGE_VERSION=latest
 
-export AWS_DEFAULT_REGION=us-east-1
+export AWS_DEFAULT_REGION=us-east-2
 export AWS_ECS_CLUSTER_NAME=default
 export AWS_VIRTUAL_HOST=18.217.139.121
 
